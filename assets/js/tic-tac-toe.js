6127be195dbf441bd071ed6b04f60d0f7cacd8ea
Omar Peart <omarpeart100@gmail.com>
2023-10-22 20:00:39 -0500

    Exercise 1

---
+++ assets/js/tic-tac-toe.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded',()=>{
+
+    const gridBox=document.getElementById("board");
+    const grid =gridBox.querySelectorAll('div');
+    grid.forEach(e=>e.className='square')
+
+
+
+})
