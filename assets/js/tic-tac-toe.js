194022c6aeba32a0c239bed9c471fb00ab40df77
Omar Peart <omarpeart100@gmail.com>
2023-10-22 21:26:01 -0500

    Exercise 4

--- assets/js/tic-tac-toe.js
+++ assets/js/tic-tac-toe.js
@@ -1,12 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
     let X = 'X';
     let O = 'O';
-    let gridArr = [];  // this is declared but not used
     let lastVal = 0;
     let playerX = true;
+    let winner = null;
 
     const gridBox = document.getElementById("board");
     const grid = gridBox.querySelectorAll('div');
+    const statusDiv = document.getElementById('status');
+
+    function checkWinner() {
+        const winningCombos = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ];
+
+        for (let combo of winningCombos) {
+            if (grid[combo[0]].innerHTML && grid[combo[0]].innerHTML === grid[combo[1]].innerHTML && grid[combo[0]].innerHTML === grid[combo[2]].innerHTML) {
+                return grid[combo[0]].innerHTML;
+            }
+        }
+
+        return null;
+    }
 
     grid.forEach(e => {
         e.className = 'square';
@@ -15,12 +32,12 @@ document.addEventListener('DOMContentLoaded', () => {
             this.classList.add('hover');
         });
 
-        e.addEventListener('mouseout', function() {
+        e.addEventListener('mouseout', function (){
             this.classList.remove('hover');
         });
 
-        e.addEventListener('click', function() {
-            if (this.hasAttribute('data-value')) {
+        e.addEventListener('click', function () {
+            if (this.hasAttribute('data-value') || winner) {
                 return;
             }
 
@@ -34,6 +51,13 @@ document.addEventListener('DOMContentLoaded', () => {
                 this.classList.add('O');
                 this.innerHTML = O;
             }
+
+            winner = checkWinner();
+            if (winner) {
+                statusDiv.textContent = `Congratulations! ${winner} is the Winner!`;
+                statusDiv.classList.add('you-won');
+            }
+
             playerX = !playerX;
         });
     });
